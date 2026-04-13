298b34c5939714ab98a3092b9653d79046394344
Eyshila2203 <eyshila.martins0308@gmail.com>
2026-04-13 20:34:32 -0300

    Add files via upload

---
+++ Projeto My Disney/Projeto.py
@@ -0,0 +1,39 @@
+print ("Seja Bem-Vindo ao MyDisney")
+
+Nome = input("Digite seu nome: ")
+Email = input("Digite seu email: ")
+Senha = input("Crie uma senha: ")
+
+Cadastro = []
+Usuário = {
+    Nome : "Nome", 
+    Email : "Email",
+    Senha : "Senha"  
+}
+
+Cadastro.append(Usuário)
+print("O usuário foi Cadastrado!")
+
+Login = input("Nome de usuário ou email: ")
+Senha = input("Senha de sua escolha: ")
+
+print (Login)
+print (Senha)
+
+print ("Estamos contentes de recebe-lo aqui! Desejamos uma que você tenha uma ótima experiência!")
+
+Catálogo = [
+    "O Compassos do Vento",
+    "Sob o Luar de Safira",
+    "Ecos de um Amor Distante",
+    "Código Infinito",
+    "Crônicas de uma Terra Esquecida",
+    "Cartas para um Destino Incerto",
+    "Operação: Namorado de Mentirinha",
+    "Adultos (Mais ou Menos)",
+    "O Clube dos Desajustados",
+    "A Garota no Lago",
+    "A Sociedade Literária e a Torta de Casca de Batata",
+    "O Paciente Silencioso"  
+]
+
