f4b41bced2a9860612a64dc4665cce9145de9557
Eyshila2203 <eyshila.martins0308@gmail.com>
2026-04-16 20:47:24 -0300

    Add files via upload

---
+++ My Disney/aula10.python/arquivo.py
@@ -0,0 +1,50 @@
+print ("Seja Bem-Vindo ao MyDisney")
+
+Nome = input("Digite seu nome: ")
+Email = input("Digite seu email: ")
+Senha = input("Crie uma senha: ")
+
+Cadastro = []
+Usuário = {
+    Nome : "Nome", 
+    Email : "Email",
+    Senha : "Senha"  
+}
+
+Cadastro.append(Usuário)
+print("O usuário foi Cadastrado!")
+
+Login = input("Nome de usuário ou email: ")
+Senha = input("Senha de sua escolha: ")
+
+print (Login)
+print (Senha)
+
+print ("Estamos contentes de recebe-lo aqui! Desejamos uma que você tenha uma ótima experiência!")
+
+Busca = input("Busque aqui: ")
+print(Busca)
+
+Catálogo = [
+    "O Compasso do Vento",
+     "Sob o Luar de Safira",
+      "Ecos de um Amor Distante",
+     "Código Infinito",
+     "Crônicas de uma Terra Esquecida",
+     "Cartas para um Destino Incerto",
+     "Operação: Namorado de Mentirinha",
+      "Adultos (Mais ou Menos)",
+      "O Clube dos Desajustados",
+     "A Garota no Lago",
+     "A Sociedade Literária e a Torta de Casca de Batata",
+     "O Paciente Silencioso"  
+]
+
+Encontrado = False
+
+if Busca in Catálogo:
+    Encontrado = True
+
+if not Encontrado:
+    print ("Item não encontrado")
+
